89cd7004a463086cd82a9d379f4a7559abaa9e8d
Mahdi Valizadeh <m.valizadeh@live.com>
2019-02-20 14:52:07 +0800

    Return the params of target from Extract Params function

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,12 +1,11 @@
 function ExtractParams(target: Function) {
     const ctor = target.prototype.constructor;
-    console.log(ctor.toString());
     const params = ctor
       .toString()
       .match(/\((?:.+(?=\s*\))|)/)[0]
       .slice(1)
       .split(/\s*,\s*/g);
-    console.log(params);
+    return params;
   }
 
   export {ExtractParams}
